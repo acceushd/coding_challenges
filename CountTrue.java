09d05ff875f036945b98eb5911d2b5ccfb599651
Roman Jost <git.juncture116@passinbox.com>
2024-10-03 17:36:04 +0200

    Added the counTrue challenge

---
+++ CountTrue.java
@@ -0,0 +1,19 @@
+public class CountTrue {
+    public int countTrue(boolean[] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+        int trues = 0;
+        for (boolean b : array) {
+            trues += b ? 1 : 0;
+        }
+        return trues;
+    }
+
+    public static void main(String[] args) {
+        CountTrue ct = new CountTrue();
+        System.out.println(ct.countTrue(new boolean[]{true, false, false, true, false}));
+        System.out.println(ct.countTrue(new boolean[]{false, false, false, false}));
+        System.out.println(ct.countTrue(new boolean[]{}));
+    }
+}
