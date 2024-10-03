3a40fe921c4e57868f3d066d00d1129e853b4634
Roman Jost <git.juncture116@passinbox.com>
2024-10-03 17:40:06 +0200

    Added more tests challenge

--- CountTrue.java
+++ CountTrue.java
@@ -12,20 +12,8 @@ public class CountTrue {
 
     public static void main(String[] args) {
         CountTrue ct = new CountTrue();
-        boolean[] array = new boolean[10000];
-        for (int i = 0; i < array.length; i += 2) {
-            array[i] = true;
-        }
-        for (int i = 1; i < array.length; i += 2) {
-            array[i] = false;
-        }
-
         System.out.println(ct.countTrue(new boolean[]{true, false, false, true, false}));
         System.out.println(ct.countTrue(new boolean[]{false, false, false, false}));
-        System.out.println(ct.countTrue(new boolean[]{true, true, true, true, true}));
-        System.out.println(ct.countTrue(new boolean[]{true, true, true, false, true}));
         System.out.println(ct.countTrue(new boolean[]{}));
-        System.out.println(ct.countTrue(new boolean[]{false, false, true, false, true}));
-        System.out.println(ct.countTrue(array));
     }
 }
